0f6e4820a417fca68ef1920c06ad466226cf990f
harishi <hvneversettle@gmail.com>
2025-05-03 23:28:34 +0200

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Add the base path for GitHub Pages deployment
+  // This should match your repository name prefixed with a slash
+  base: '/GDG_HACKATHON/',
+
   plugins: [
     vue(),
     vueDevTools(),
